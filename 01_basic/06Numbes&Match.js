2b479d8259f8f3127f752004ff14fd852acf41c6
Shreyashj9600 <139689092+Shreyashj9600@users.noreply.github.com>
2024-08-22 08:03:36 +0000

    numbers&Match.js

---
+++ 01_basic/06Numbes&Match.js
@@ -0,0 +1,9 @@
+const score = 400
+console.log("this is my score value 1",typeof score);
+
+const score2 = "400"
+console.log("this is my second value 2", typeof score2);
+
+const score3 = new Number(400)
+console.log("this is my thired value 3", typeof score3);
+
--- 01_basic/test.js
+++ 01_basic/test.js
@@ -1,1 +1,8 @@
 console.log("hello world !..");
+
+
+
+let str = "https://github.com/Shreyashj9600/JS_Repo"
+
+let result = str.split("/")
+console.log(result);
