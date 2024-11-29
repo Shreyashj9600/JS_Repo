b97ac425dce27cd7c71b7542cc822085774e7377
Shreyashj9600 <139689092+Shreyashj9600@users.noreply.github.com>
2024-11-29 03:20:14 +0000

    scope

--- 03_basic/01_functions.js
+++ 03_basic/01_functions.js
@@ -8,21 +8,27 @@ function sayMyName() {
     console.log("s");
 }
 
-// sayMyName();
+// sayMyName()
 
-// Add to numbers
-
-function addNo(num1, num2) {
-    console.log(num1 + num2);
-}
-// addNo(2, 4);
-
-
-function userDetails(anyObject) {
-    console.log(  `user Name is ${anyObject.usename} and price is ${anyObject.price}`)
+function addTwoNumbers(num1, num2){
+    console.log(num1 + num2)
 }
 
-userDetails({
-    usename: "rohit",
-    price: 999
-})
+addTwoNumbers(2 ,2 )
+
+function loginUserMassage(usernaem ='test'){
+    if(usernaem === undefined){
+        console.log('please enter a username ')
+        return 
+    }
+    return `${usernaem} just logged in`
+}
+
+console.log(loginUserMassage('shreyash'))
+
+
+function calculateCartPrice (...num1) {
+    return num1;
+}
+
+console.log(calculateCartPrice(2,3,4,5,6,7))
