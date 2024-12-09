1a7c0903a0f7601d2a2a04ff37c6ebc0372130b3
Shreyashj9600 <139689092+Shreyashj9600@users.noreply.github.com>
2024-12-09 18:27:44 +0000

    hosting js

--- 03_basic/01_functions.js
+++ 03_basic/01_functions.js
@@ -1,34 +1,46 @@
 function sayMyName() {
-    console.log("s");
-    console.log("s");
-    console.log("s");
-    console.log("s");
-    console.log("s");
-    console.log("s");
-    console.log("s");
+  console.log("s");
+  console.log("s");
+  console.log("s");
+  console.log("s");
+  console.log("s");
+  console.log("s");
+  console.log("s");
 }
 
 // sayMyName()
 
-function addTwoNumbers(num1, num2){
-    console.log(num1 + num2)
+function addTwoNumbers(num1, num2) {
+  console.log(num1 + num2);
 }
 
-addTwoNumbers(2 ,2 )
+addTwoNumbers(2, 2);
 
-function loginUserMassage(usernaem ='test'){
-    if(usernaem === undefined){
-        console.log('please enter a username ')
-        return 
-    }
-    return `${usernaem} just logged in`
+function loginUserMassage(usernaem = "test") {
+  if (usernaem === undefined) {
+    console.log("please enter a username ");
+    return;
+  }
+  return `${usernaem} just logged in`;
 }
 
-console.log(loginUserMassage('shreyash'))
+console.log(loginUserMassage("shreyash"));
 
-
-function calculateCartPrice (...num1) {
-    return num1;
+function calculateCartPrice(...num1) {
+  return num1;
 }
 
-console.log(calculateCartPrice(2,3,4,5,6,7))
+console.log(calculateCartPrice(2, 3, 4, 5, 6, 7));
+
+function one() {
+  let username = "shreyash";
+
+  function two() {
+    let website = "youtube";
+    console.log(username);
+  }
+  console.log(website);
+
+  two();
+}
+one();
